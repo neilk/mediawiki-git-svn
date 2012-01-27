02042c0c857e0255e2f7de9ae1a77a5d5827d8b2
Neil Kandalgaonkar <neilk@brevity.org>
2012-01-26 18:48:55 -0800

    refactored to allow installing arbitrary extensions

--- gitSvnExtsFromLs.pl
+++ gitSvnExtsFromLs.pl
@@ -1,49 +1,94 @@
 #!/usr/bin/env perl
 
 # execute this script in $IP, i.e. the dir containing your MediaWiki LocalSettings.php
-# creates git repos for each of the extensions required in LocalSettings.php, under $IP/extensionsGit
-# then do whatever you need to make extensionsGit your extensions dir
-# if you are placing it inside another git repo, you may need to add it to .gitignore
+#
+# with arguments on the command line, will simply install those extensions into extensions
+# 
+# if executed with flag --localsettings then it will obtain a list of extensions to get from LocalSettings.php
+# and create the extensions in $IP/extensionsgit
+
 
 use strict;
 use warnings qw/all/;
 
 use Cwd qw/getcwd/;
+use Getopt::Long;
 
 # configuration
-my $user = 'neilk';
 my $minRev = 50000;
 
 # all else should be good below, if you have a typical layout
 debugSetup();
 
+my $user = $ENV{"USER"};
 my $ipDir = getcwd();
-my $gitExtDir = "$ipDir/extensionsGit";
+my $extDir = "$ipDir/extensions";
 my $baseUrl = "svn+ssh://$user\@svn.wikimedia.org/svnroot/mediawiki/trunk/extensions";
 
-makeExtReposFromLocalSettings( $ipDir, $gitExtDir, $baseUrl, $minRev );
+my $localSettings = 0;
+
+my $result = GetOptions( 
+  "user=s" => \$user,
+  "ip=s" => \$ipDir, 
+  "extdir=s" => \$extDir,
+  "baseurl=s" => \$baseUrl,
+  "user=s" => \$user,
+  "minrev=i" => \$minRev,
+  "localsettings" => \$localSettings,
+);
+
+if ( $localSettings ) {  
+  makeExtReposFromLocalSettings( $ipDir, $extDir, $baseUrl, $minRev );
+} else {
+  makeExtRepos( \@ARGV, $baseUrl, $extDir, $minRev );
+}
+
+# the end
+
 
 
 sub makeExtReposFromLocalSettings {
-  my ($ipDir, $gitExtDir, $baseUrl, $minRev) = @_;
+  my ($ipDir, $extDir, $baseUrl, $minRev) = @_;
+  my @extFromLocalSettings = getExtensionsFromLocalSettings( $ipDir );
+  makeExtRepos( \@extFromLocalSettings, $baseUrl, $extDir . "git", $minRev );
+}
 
-  my %ext = getExtensionsRequiredEarliestRevision( $ipDir, $baseUrl );
-   
-  if ( ! -d $gitExtDir ) {
-    mkdir $gitExtDir or die $!;
+sub getExtensionsFromLocalSettings { 
+  my ($ipDir) = @_; 
+  my @ext = ();
+  my $localSettingsFile = "$ipDir/LocalSettings.php";
+  open my $fh, '<', $localSettingsFile or die "can't open LocalSettings -- $!";
+  while (<$fh>) {
+    if ( $_ =~ /require(?:_once)\s*\(.*\/extensions\/(\w+)/ ) {
+      push @ext, $1;
+    }
   }
-
-  while ( my ( $ext, $props ) = each %ext ) {
-    debug( ">> make git repo for $ext" );
-    makeGitRepo( $gitExtDir, $ext, $props, $minRev );
-  }
+  close $fh or die $!;
+  debug( "required extensions: " . (join ", ", @ext ) . "\n" );
+  return @ext;
 }
 
 
-sub getExtensionsRequiredEarliestRevision {
-  my ($ipDir, $baseUrl) = @_;
+sub makeExtRepos {
+  my ( $exts, $baseUrl, $extDir, $minRev ) = @_;
+ 
+  if ( ! -d $extDir ) {
+    mkdir $extDir or die $!;
+  }
+
+  my %ext = getExtensionsEarliestRevision( $exts, $baseUrl );
+
+  while ( my ( $ext, $props ) = each %ext ) {
+    debug( ">> make git repo for $ext" );
+    makeGitRepo( $extDir, $ext, $props, $minRev );
+  }
+
+}
+
+sub getExtensionsEarliestRevision {
+  my ($exts, $baseUrl) = @_;
   my %extRev = ();
-  foreach my $ext ( getExtensionsRequired( $ipDir ) ) {
+  foreach my $ext ( @$exts ) {
     my $extUrl = "$baseUrl/$ext";
     my $rev = getEarliestRev( $extUrl );
     if ( ! defined $rev ) {
@@ -70,31 +115,17 @@ sub getEarliestRev {
   return $rev;
 }
 
-sub getExtensionsRequired { 
-  my ($ipDir) = @_; 
-  my @ext = ();
-  open my $fh, '<', "$ipDir/LocalSettings.php" or die "can't open LocalSettings -- $!";
-  while (<$fh>) {
-    if ( $_ =~ /require(?:_once)\s*\(.*\/extensions\/(\w+)/ ) {
-      push @ext, $1;
-    }
-  }
-  close $fh or die $!;
-  debug( "required extensions: " . (join ", ", @ext ) . "\n" );
-  return @ext;
-}
-
 sub makeGitRepo {
-  my ($gitExtDir, $ext, $props, $minRev) = @_;
+  my ($extDir, $ext, $props, $minRev) = @_;
   my $url = $props->{'url'};
   my $rev = $props->{'rev'};
-  chdir $gitExtDir or die $!;
+  chdir $extDir or die $!;
   $rev = int($rev);
   if ( $rev < $minRev ) {
     $rev = $minRev;
   }
   sys( "git svn clone -r $rev:HEAD $url" );
-  chdir "$gitExtDir/$ext" or die $!;
+  chdir "$extDir/$ext" or die $!;
   sys( "git svn rebase " );
 }
 
